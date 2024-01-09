7b1fc226f871828f767f78d1d8aebd51428ada32
eschoonewolff <eschoonewolff@ises.com.co>
2024-01-09 12:48:48 -0500

    cambios

--- app/parametros/gerencia/gerencia_router.py
+++ app/parametros/gerencia/gerencia_router.py
@@ -6,19 +6,6 @@ from fastapi.responses import JSONResponse
 from app.parametros.gerencia.gerencia_servicio import Gerencia
 
 gerencia = APIRouter(tags=['Gerencia'])
-
-
-# @gerencia.get("/obtener")
-# def obtener_gerencia():
-#     try:
-#         gerenciaModelo = Gerencia()
-#         informacion_gerencia = gerenciaModelo.obtener()
-#         status_code = 200
-#         return JSONResponse(content=informacion_gerencia, status_code=status_code)
-#     except Exception as e:
-#         print(e)
-#         return JSONResponse(content={"error": str(e)}, status_code=500)
-    
 @gerencia.post("/subir-archivo")
 async def subir_archivo(file: UploadFile = File(...)):
     try:
@@ -45,7 +32,3 @@ async def subir_archivo(file: UploadFile = File(...)):
     except Exception as e:
         print(e)
         return JSONResponse(content={"error": str(e)}, status_code=500)
-
-
-
-
